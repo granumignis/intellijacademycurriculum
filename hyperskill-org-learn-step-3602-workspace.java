aaa46af9a8c76a58143adfe02c2e2077548588c6
lukecool@gmail.com <lukecool@gmail.com>
2020-06-09 20:15:29 -0500

    a 'bookmark' screenshot and a new scratch file

---
+++ hyperskill-org-learn-step-3602-workspace.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        int sizeOfArray = scanner.nextInt();
+        int[] array = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+
+
+
+    }
+}
