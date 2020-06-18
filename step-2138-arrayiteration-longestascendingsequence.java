ebd00ce1bd27ce1309c59ee775065aec4fb74fb1
lukecool@gmail.com <lukecool@gmail.com>
2020-06-17 20:14:39 -0500

    adding more loops, getting closer to desired behavior

--- step-2138-arrayiteration-longestascendingsequence.java
+++ step-2138-arrayiteration-longestascendingsequence.java
@@ -6,18 +6,32 @@ class Main {
         Scanner s = new Scanner(System.in);
         int ArraySize = s.nextInt();
         int[] array = new int[ArraySize];
+        int ascendingSequenceCounter = 0;
+        int lastAscendingSequenceCount = 0;
 
         for (int i = 0; i < ArraySize; i++) {
+            array[i] = s.nextInt();
+
             if (i > 0 && (array[i] > array[i-1])) {
                 System.out.print("The current array value of " + String.valueOf(array[i]));
                 System.out.print("is bigger than the previous array value of " + String.valueOf(array[i-1] + "\n"));
+                ascendingSequenceCounter++;
+
             } else if (i > 0) {
+
+                if (ascendingSequenceCounter > lastAscendingSequenceCount) {
+                    lastAscendingSequenceCount = Integer.valueOf(ascendingSequenceCounter);
+                }
+                ascendingSequenceCounter = 0;
+
                 System.out.println("i: " + String.valueOf(i));
                 System.out.println("array[i]:" + String.valueOf(array[i]));
                 System.out.println("array[i-1]:" + String.valueOf(array[i-1]));
             }
 
-            array[i] = s.nextInt();
+            System.out.println("ascendingSequenceCounter: " + ascendingSequenceCounter);
+            System.out.println("lastAscendingSequenceCount: " + lastAscendingSequenceCount);
+
         }
 
 
@@ -34,6 +48,8 @@ class Main {
         when curnum !> prevnum, set the counter back to 0?
 
         Ya! ^^^
+
+        Sort of
 
         ==
 
