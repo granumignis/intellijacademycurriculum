79ac32c171e024a81f9beaa260b7e753a0d09008
lukecool@gmail.com <lukecool@gmail.com>
2020-06-22 21:10:21 -0500

    Sample Input 1 produces the desire output, but 'test 2' is failing

--- step-2138-arrayiteration-longestascendingsequence.java
+++ step-2138-arrayiteration-longestascendingsequence.java
@@ -10,11 +10,16 @@ class Main {
         int lastAscendingSequenceCount = 0;
 
         for (int i = 0; i < ArraySize; i++) {
+            //System.out.print("**For Loop Begin**\n");
             array[i] = s.nextInt();
 
+            if (i == 0) {
+                ascendingSequenceCounter++;
+            }
+
             if (i > 0 && (array[i] > array[i-1])) {
-                System.out.print("The current array value of " + String.valueOf(array[i]));
-                System.out.print("is bigger than the previous array value of " + String.valueOf(array[i-1] + "\n"));
+                //System.out.print("The current array value of " + String.valueOf(array[i]));
+                //System.out.print(" is bigger than the previous array value of " + String.valueOf(array[i-1] + "\n\n"));
                 ascendingSequenceCounter++;
 
             } else if (i > 0) {
@@ -22,35 +27,27 @@ class Main {
                 if (ascendingSequenceCounter > lastAscendingSequenceCount) {
                     lastAscendingSequenceCount = Integer.valueOf(ascendingSequenceCounter);
                 }
-                ascendingSequenceCounter = 0;
+                ascendingSequenceCounter = 1;
 
-                System.out.println("i: " + String.valueOf(i));
-                System.out.println("array[i]:" + String.valueOf(array[i]));
-                System.out.println("array[i-1]:" + String.valueOf(array[i-1]));
+
             }
 
-            System.out.println("ascendingSequenceCounter: " + ascendingSequenceCounter);
-            System.out.println("lastAscendingSequenceCount: " + lastAscendingSequenceCount);
+            //System.out.println("i: " + String.valueOf(i));
+            //System.out.println("array[i]:" + String.valueOf(array[i]));
+            //if (i != 0) {
+                //System.out.println("array[i-1]:" + String.valueOf(array[i - 1]));
+            //}
+            //System.out.println("ascendingSequenceCounter: " + ascendingSequenceCounter);
+            //System.out.println("lastAscendingSequenceCount: " + lastAscendingSequenceCount);
 
+
+
+        //System.out.print("**For Loop End**\n\n");
         }
+
+        System.out.println(lastAscendingSequenceCount);
 
 
 
     }
 }
-
-/*
-
-    Working theory
-
-==
-        increment a counter or something when  curnum > prevnum
-        when curnum !> prevnum, set the counter back to 0?
-
-        Ya! ^^^
-
-        Sort of
-
-        ==
-
-*/
