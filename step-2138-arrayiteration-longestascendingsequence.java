9fca1d0314306f49c8352a8424fe3d57aadbdd4b
lukecool@gmail.com <lukecool@gmail.com>
2020-06-24 23:28:50 -0500

    Completed Step 2138! - 'Longest Ascending Sequence'

--- step-2138-arrayiteration-longestascendingsequence.java
+++ step-2138-arrayiteration-longestascendingsequence.java
@@ -4,12 +4,12 @@ class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int ArraySize = s.nextInt();
-        int[] array = new int[ArraySize];
+        int arraySize = s.nextInt();
+        int[] array = new int[arraySize];
         int ascendingSequenceCounter = 0;
         int lastAscendingSequenceCount = 0;
 
-        for (int i = 0; i < ArraySize; i++) {
+        for (int i = 0; i < arraySize; i++) {
             //System.out.print("**For Loop Begin**\n");
             array[i] = s.nextInt();
 
@@ -17,9 +17,9 @@ class Main {
                 ascendingSequenceCounter++;
             }
 
-            if (i > 0 && (array[i] > array[i-1])) {
+            if (i > 0 && array[i] > array[i - 1]) {
                 //System.out.print("The current array value of " + String.valueOf(array[i]));
-                //System.out.print(" is bigger than the previous array value of " + String.valueOf(array[i-1] + "\n\n"));
+                //System.out.print(" is > than the previous array value of " + String.valueOf(array[i-1] + "\n\n"));
                 ascendingSequenceCounter++;
 
             } else if (i > 0) {
@@ -35,19 +35,23 @@ class Main {
             //System.out.println("i: " + String.valueOf(i));
             //System.out.println("array[i]:" + String.valueOf(array[i]));
             //if (i != 0) {
-                //System.out.println("array[i-1]:" + String.valueOf(array[i - 1]));
+            //System.out.println("array[i-1]:" + String.valueOf(array[i - 1]));
             //}
             //System.out.println("ascendingSequenceCounter: " + ascendingSequenceCounter);
             //System.out.println("lastAscendingSequenceCount: " + lastAscendingSequenceCount);
 
 
 
-        //System.out.print("**For Loop End**\n\n");
+            //System.out.print("**For Loop End**\n\n");
         }
 
-        System.out.println(lastAscendingSequenceCount);
+        if (ascendingSequenceCounter >= lastAscendingSequenceCount) {
+            System.out.println(ascendingSequenceCounter);
+        }
 
-
+        if (lastAscendingSequenceCount > ascendingSequenceCounter) {
+            System.out.println(lastAscendingSequenceCount);
+        }
 
     }
 }
