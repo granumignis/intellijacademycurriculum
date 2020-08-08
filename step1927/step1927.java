b2916de6a424aec6e8d2a2e0bacf96844624179b
lukecool@gmail.com <lukecool@gmail.com>
2020-08-07 20:26:37 -0500

    creation and output of empty rotated array

--- step1927/step1927.java
+++ step1927/step1927.java
@@ -2,52 +2,55 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-        int n = 0;
-        Scanner scanner = new Scanner(System.in);
+        int n = s.nextInt(); // y axis
+        int m = s.nextInt(); // x axis
 
-        n = scanner.nextInt();
+        String[][] theArray = new String[n][m];
 
-        while (n % 2 != 0 && n > 15) {
-            n = scanner.nextInt();
-        }
-        String[][] array = new String[n][n]; // create a two-dimensional array of 'n' by 'n'
-        int middle = (n - (n / 2)) - 1;
+        for (int i = 0; i < n; i++) {
 
-        int reverseDiagonalI = middle - middle;
-        int reverseDiagonalJ = middle * 2;
-
-        // Define and Fill NxN array
-        for (int i = 0; i < n; i++) { // this is defining the 'x axis' of the array
-            if (i > 0) {
-                reverseDiagonalI = reverseDiagonalI + 1;
-                reverseDiagonalJ = reverseDiagonalJ - 1;
-            }
-            for (int j = 0; j < n; j++) { // this is defining the 'y axis' of the array
-                if (i == middle) {
-                    array[i][j] = "*"; // This is filling each 'row' of the NxN array with values
-                } else if (j == middle) {
-                    array[i][j] = "*"; // This is filling each 'row' of the NxN array with values
-                } else {
-                    if (i == j) {
-                        array[i][j] = "*"; // This is filling each 'row' of the NxN array with values
-                    } else if (i == reverseDiagonalI && j == reverseDiagonalJ) {
-                        array[i][j] = "*"; // This is filling each 'row' of the NxN array with values
-                    } else {
-                        array[i][j] = "."; // This is filling each 'row' of the NxN array with values
-                    }
-                }
-            }
-        }
-
-        // Output the array contents
-        for (int i = 0; i < n; i++) { // loop with variable 'i' until 'i' is one less than 'n'
-
-            for (int j = 0; j < n; j++) { // loop with variable 'j' until 'j' is one less than 'n'
-                System.out.print(array[i][j] + " "); // print out the value of the current 'Vector2'
+            for (int j = 0; j < m; j++) {
+                theArray[i][j] = "*";
             }
 
-            System.out.println(); // print a line break after printing out all values for the current array ('i')
         }
+
+
+        //Output Original Array
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                System.out.print(theArray[i][j]);
+            }
+
+            System.out.println();
+
+        }
+
+        // Create Rotated Array
+
+        String[][] theRotatedArray = new String[m][n];
+
+        // Output Rotated Array
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+                System.out.print(theRotatedArray[i][j]);
+            }
+
+            System.out.println();
+
+        }
+
+
+
+
+
+
+
     }
 }
