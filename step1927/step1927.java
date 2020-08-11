dc852ce4afafe399ec7e2015806038279ce1a9e7
lukecool@gmail.com <lukecool@gmail.com>
2020-08-10 20:41:23 -0500

    added some rough notes, trying to figure out how to translate the array 'coordinates'

--- step1927/step1927.java
+++ step1927/step1927.java
@@ -1,3 +1,35 @@
+/*
+
+
+
+==================
+
+
+Rotation Test (this is all coordinates, not values):
+
+0,0 becomes 0,3
+0,1 becomes 1,3
+0,2 becomes 1,4
+
+
+So, what is the formula to translate these co-ordinates
+
+==================
+
+
+Formula:
+
+rotatedarray[0,3] = originalarray[0,0]
+
+So, translate the x coordinate of the orignal array by ??
+
+Translate the y coordinate of the original array by ??
+
+=================
+
+
+ */
+
 import java.util.Scanner;
 
 class Main {
